d9beeee7351b7317e4e45257de462e524317234b
Mangla-Ram <115937699+Mangla-Ram@users.noreply.github.com>
2023-12-28 23:56:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+    function submitform() {
+        const fname = document.getElementById('fname');
+        const phone = document.getElementById('phone');
+        const email = document.getElementById('email');
+        const subject = document.getElementById('Subject');
+        const Message = document.getElementById('Message');
+        console.log(fname.value);
+        console.log(phone.value);
+        console.log(email.value);
+        console.log(subject.value);
+        console.log(Message.value);
+    }
